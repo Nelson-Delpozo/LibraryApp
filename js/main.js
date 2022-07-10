e972f0798d017933afc98c5425e693d602ab02bb
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-07-09 20:39:48 -0500

    added books

--- js/main.js
+++ js/main.js
@@ -9,3 +9,7 @@ function addBookToLibrary(book){
   myLibrary.push(book)
 }
 
+let book1 = new Book("For Whom the Bell Tolls", "Hemingway")
+let book2 = new Book("On the Road", "Kerouac")
+let book3 = new Book("Journey to the East", "Hesse")
+
