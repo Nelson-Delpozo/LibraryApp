0028eb6ec9cb7e9c27c61381d7251b528b74bd59
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-07-09 20:58:25 -0500

    logging book titles

--- js/main.js
+++ js/main.js
@@ -13,6 +13,14 @@ let book1 = new Book("For Whom the Bell Tolls", "Hemingway")
 let book2 = new Book("On the Road", "Kerouac")
 let book3 = new Book("Journey to the East", "Hesse")
 
-for(let i = 0; i < myLibrary.length; i++){
-
+function showBooks(array) {
+  for (let i = 0; i < myLibrary.length; i++) {
+    console.log(array[i].title)
+  }
 }
+
+addBookToLibrary(book1)
+addBookToLibrary(book2)
+addBookToLibrary(book3)
+
+showBooks(myLibrary);
