e5b08f8f209e362aa9fb2a135e1db469a8edc552
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-07-16 14:07:13 -0500

    warmups

--- js/codewars.js
+++ js/codewars.js
@@ -2,3 +2,12 @@ function doubleInteger(i) {
   // i will be an integer. Double it and return it.
   return i * 2;
 }
+
+function makeUpperCase(str) {
+  // Code here
+  return str.toUpperCase()
+}
+console.log(makeUpperCase("hkhkhkjh"));
+
+
+
