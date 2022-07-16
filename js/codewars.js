ca035d409dbda4300ae334765e3f9df30725052c
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-07-16 14:41:40 -0500

    reducing i mid-loop

--- js/codewars.js
+++ js/codewars.js
@@ -15,6 +15,21 @@ function check(a, x) {
 console.log(check("hello", "e"))
 console.log(check("hello", "f"))
 
+function filter_list(l) {
+  // Return a new array with the strings filtered out
+  for(let i = 0; i < l.length; i++){
+    if(typeof l[i] == 'string'){
+      l.splice(i, 1);
+      i -= 1;
+    }
+  } return (l);
+    }
+
+// function filter_list(l) {
+//   return l.filter(function(v) {return typeof v == 'number'})
+// }
+
+console.log(filter_list(['hj', 8, 'jiji', 9, 'hi there', 17, "b", "c", 190, "d", "f"]));
 
 
 
