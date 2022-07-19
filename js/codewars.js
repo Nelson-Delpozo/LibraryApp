f571387c86aa510479ef299b9640ab9e03eaf948
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-07-19 11:59:38 -0500

    target number

--- js/codewars.js
+++ js/codewars.js
@@ -126,6 +126,19 @@ function validParentheses(parens) {
   }else {
     return false;
   }
-
 }
 
+function twoSum(numbers, target) {
+  // ...
+  let result = [];
+
+  for(let i = 0; i < numbers.length; i++){
+    for(let j = i + 1; j < numbers.length; j++){
+      if(numbers[i] + numbers[j] === target){
+        result.push(i)
+        result.push(j)
+      }
+    }
+
+  }return result;
+}
